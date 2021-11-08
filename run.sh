52ef98e6802510454651a48d49099456971d3936
iserra <i.serra@.creaf.uab.cat>
2021-11-08 16:28:51 +0100

    changes

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 #read -p 'Start date: ' file1
 #read -p 'Endd date: ' file2
 
-read data1
-read data2
-
+var file1
+var file2
+echo 'hola'
 #python test.py $f1 $f2
-echo $data1
-echo $data2
+echo $file1
+echo file1
 
-python test.py $data1 $data2
+python test.py $file1 file2
 
 #python test.py $file1 $file2
 
@@ -19,7 +19,7 @@ ls -l img
 ls -d img
 find  img -print
 find  -name '*.SAFE'
-mv img $img
+find -name 'manifest.safe'
 
 
 #unzip '*.zip' -d /tmp/unzipped
