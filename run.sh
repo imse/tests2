0d408386e2bc5a363a9075010b116eee0a552932
iserra <i.serra@.creaf.uab.cat>
2021-11-09 11:38:37 +0100

    changes run script

--- run.sh
+++ run.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-#read -p 'Start date: ' file1
-#read -p 'Endd date: ' file2
+read  file1  # read -p 'Start date: ' file1
+read  file2
 
 #var file1 # var: command not found
 
-echo 'hola'  #ok
+#FILE=/s2png/test.py
+
+#if [ -f "$FILE" ]; then
+#    echo "$FILE exists."
+#else
+#    echo "$FILE does not exist."
+#fi
+
+echo ${file1}  #ok
+echo "hola"
 #python test.py $f1 $f2
 #echo ${file1}
 #echo file1 #print file1 as string
 
 #python test.py $1 $2
-python test.py file1 file2
+python test.py ${file1} ${file2}
 
 #file2 = sys.argv[2]
 #IndexError: list index out of range
 #python test.py $file1 $file2
 
 #debug lines
-ls -l img
-ls -d img
+##ls -l img
+##ls -d img
 find  img -print
 find  -name '*.SAFE'
-find -name 'manifest.safe'
-
 
 #unzip '*.zip' -d /tmp/unzipped
 #source activate snappyEnv
